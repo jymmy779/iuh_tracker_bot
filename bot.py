62880b5c6118a100112df29c6fa6f77924ef3bc9
thaigithurb <jymmynguyen103@gmail.com>
2026-08-06 23:40:42 +0700

    Fix Python 3.14 event loop

--- bot.py
+++ bot.py
@@ -250,6 +250,8 @@ def main():
 
 if __name__ == "__main__":
     try:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
         main()
     except KeyboardInterrupt:
         logger.info("Bot đã bị dừng.")
